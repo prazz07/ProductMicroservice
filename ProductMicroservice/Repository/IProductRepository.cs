d49001e30ce081695788249bbfc8530ee270638f
Pranjal Agarwal <INT141@xceedance.com>
2020-06-22 19:13:12 +0530

    Initial commit

---
+++ ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using ProductMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int ProductId);
+        void InsertProduct(Product Product);
+        void UpdateProduct(Product Product);
+        void DeleteProduct(int ProductId);
+        void Save();
+
+    }
+}
